a59ba069cb03d752cb82145847e4552a882e142f
Chengzhang <74751781+ChengzhangBai@users.noreply.github.com>
2022-07-16 08:33:48 -0400

    Add project files.

---
+++ Tetris/Block.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    public abstract class Block
+    {
+        //Before you can use jaggedArray, its elements must be initialized
+        //int[][] jaggedArray2 = new int[][]
+        //{
+        //new int[] { 1, 3, 5, 7, 9 },
+        //new int[] { 0, 2, 4, 6 },
+        //new int[] { 11, 22 }
+        //};
+        protected abstract Position[][] Tiles { get; } //A jagged array is an array whose elements are arrays, possibly of different sizes.
+        protected abstract Position StartOffset { get; }
+        public abstract int Id { get; }
+        private int rotationState;
+        private Position offset; //
+
+        public Block()
+        {
+            offset = new Position(StartOffset.Row, StartOffset.Column);
+        }
+
+        public IEnumerable<Position> TilePositions()
+        {
+            foreach (Position p in Tiles[rotationState]) 
+            {
+            yield return new Position(p.Row+offset.Row, p.Column+offset.Column);
+            }
+        }
+
+        public void RotateCW()
+        {
+            rotationState = (rotationState+1) % Tiles.Length; //5%3=2, 100%90=10；2%5=2;5%5=0
+        }
+
+        public void RotateCCW()
+        {
+            if (rotationState==0)
+            {
+                rotationState = Tiles.Length-1; //交错数组的长度为其行数
+            }
+            else
+            {
+                rotationState--;
+            }
+        }
+        public void Move(int rows, int columns)
+        {
+            offset.Row += rows;
+            offset.Column += columns;
+        }
+        public void Reset()
+        {
+            rotationState=0;
+            offset.Row = StartOffset.Row;
+            offset.Column = StartOffset.Column;
+        }
+    }
+}
---
+++ Tetris/GameGrid.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    public class GameGrid
+    {
+        private readonly int[,] grid;//二维数组
+        public int Rows { get; }
+        public int Columns { get; }
+        public int this[int r, int c] //indexer 允许一个对象可以像数组一样使用下标的方式来访问。
+        {
+            get=> grid[r,c];
+            set=>grid[r,c] = value;
+        }
+        public GameGrid(int rows, int columns) //构造函数：无返回类型的方法
+        {
+            Rows = rows;
+            Columns = columns;
+            grid = new int[rows, columns]; //if value of grid[r,c] equals 0, the grid is empty
+        }
+        public bool IsInside(int r, int c)
+        {
+            return r>=0 &&r<Rows && c>=0 &&c<Columns;
+        }
+
+        public bool IsEmpty(int r, int c)
+        {
+            return IsInside(r,c) && grid[r,c]==0;//get value by a indexer
+        }
+        public bool IsRowFull(int r) 
+        {
+            for (int c=0;c<Columns;c++)
+            {
+                if (grid[r,c]==0)
+                {
+                    return false;
+                }
+                
+            }
+            return true;
+        }//if row full, then this row should be cleared and the others be moved down by one row.
+
+        public bool IsRowEmpty(int r) //if isRowEmpty, then this row above should be moved down
+        {
+            for (int c = 0;c<Columns;c++)
+            {
+                if (grid[r,c]!=0) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private void ClearRow(int r)
+        {
+            for (int c=0;c<Columns;c++)
+            {
+                grid[r, c] = 0;  //whole row will be 0
+            }
+        }
+
+        private void MoveRowDown(int r, int numRows)
+        {
+            for (int c=0;c<Columns;c++)
+            {
+                grid[r + numRows, c] = grid[r, c];//the next row will be the same as above
+                grid[r, c] = 0; //the above row will be emptied.
+            }
+        }
+
+        public int ClearFullRows() //返回清除满格行的数量
+        {
+            int cleared = 0;
+            for (int r=Rows-1;r>=0;r--) //from bottom to up
+            {
+                if (IsRowFull(r))
+                {
+                    ClearRow(r);
+                    cleared++;
+                }
+                else if (cleared>0) 
+                {
+                MoveRowDown(r,cleared);
+                }
+            }
+            return cleared;
+        }
+
+    }
+}
